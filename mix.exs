b47b5a2304059d1ac61ef3cecce66a60266a4bad
John Colagioia <john@colagioia.net>
2016-03-13 07:45:24 -0400

    Fix dependency inconsistency

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule CardPlayers.Mixfile do
   defp deps do
     [{:phoenix, "~> 0.17"},
      {:phoenix_ecto, "~> 1.1"},
-     {:postgrex, ">= 0.0.0"},
+     {:postgrex, ">= 0.9.1"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
